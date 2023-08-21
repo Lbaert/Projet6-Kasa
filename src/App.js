7c986293888b25a329aba25745633deb8f28375b
PC <lbaert.pro@gmail.com>
2023-08-21 17:13:41 +0200

    up

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
-import NotFound from './pages/notFound/NotFound'
+import NotFound from './pages/notFound/NotFound';
 import Logement from './pages/logement/Logement';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/logement" element={<Logement />} />
-          <Route path="*" element={<NotFound />} /> 
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logement/:logementId" element={<Logement />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,23 +1,24 @@
-import './gallery.scss'
-import Card from '../card/Card'
-import { logementList } from '../../data/data'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './gallery.scss';
+import Card from '../card/Card';
+import { logementList } from '../../data/data';
 
 export default function Gallery() {
-
-    return (
-        <main className="gallery">
-            <h2 className='hidden'>Nos appartements</h2>
-            <div className='card'>
-                    {logementList.map((Logement) => (
-                            <Card 
-                                key={Logement.id}
-                                id={Logement.id}
-                                title={Logement.title}
-                                img={Logement.cover}
-                            />
-                            ))
-                    }
-            </div>
-        </main>
-    )
+  return (
+    <main className="gallery">
+      <h2 className="hidden">Nos appartements</h2>
+      <div className="card">
+        {logementList.map((logement) => (
+          <Link key={logement.id} to={`/logement/${logement.id}`} className="card-link">
+            <Card
+              id={logement.id}
+              title={logement.title}
+              img={logement.cover}
+            />
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
 }
