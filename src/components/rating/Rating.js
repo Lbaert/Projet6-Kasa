480a7ec02528ba64d5f3e3789638425eb96ffb39
PC <lbaert.pro@gmail.com>
2023-09-01 14:51:29 +0200

    changement icon rating

--- src/components/rating/Rating.js
+++ src/components/rating/Rating.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './rating.scss';
+import filledStarImage from '../../img/vectorFULL.png';
+import emptyStarImage from '../../img/vector.png'; 
 
 export default function Rating({ rating }) {
   const maxRating = 5;
@@ -7,9 +9,9 @@ export default function Rating({ rating }) {
   return (
     <div className="rating">
       {Array.from({ length: maxRating }).map((_, index) => (
-        <span key={index} className={`star ${index < rating ? 'filled' : ''}`}>
-          {index < rating ? '★' : '☆'}
-        </span>
+        <span key={index} className="star">
+        {index < rating ? <img src={filledStarImage} alt="Filled Star" /> : <img src={emptyStarImage} alt="Empty Star" />}
+      </span>
       ))}
     </div>
   );
